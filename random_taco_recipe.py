f8dacdca93ca594c4733c2a368a2f72f2d641cf6
Enigma227 <34309198+Enigma227@users.noreply.github.com>
2019-12-04 11:49:36 -0600

    Added important comments

--- random_taco_recipe.py
+++ random_taco_recipe.py
@@ -4,3 +4,15 @@ from PIL import Image
 
 # I start by importing the needed libraries
 
+'''image = Image.open('taco.jpg')
+
+width = image.width
+
+height = image.height
+
+print(width, height)
+
+I then use the commented out code to find the proportions of the image to change by a ratio of 7.32 to get the
+max size to 800 px'''
+
+
