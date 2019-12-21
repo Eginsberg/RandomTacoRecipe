7dc2d8c68ea45bfd01f1f402c8979ba6d06dae0a
Enigma227 <34309198+Enigma227@users.noreply.github.com>
2019-12-20 19:45:02 -0600

    adding image

--- random_taco_recipe.py
+++ random_taco_recipe.py
@@ -21,10 +21,20 @@ max size to 800 px'''
 scaled_width = int(width / 7.3225)
 scaled_height = int(height / 7.3225)
 
+# I then scaled the size down to meet the standards
+
 image = image.resize((scaled_width, scaled_height))
+
+# remade the image
 
 img_draw = ImageDraw.Draw(image)
 font = ImageFont.truetype('arial.ttf', 50)
 img_draw.text([120, 20], 'Random Taco Cookbook', fill='black', font=font)
 
+# I then wrote the words random taco cookbook on the image
+
 image.save('new_taco.jpg')
+
+# I then saved the image
+
+data = requests.get('https://taco-1150.herokuapp.com/random/?full_taco=true').json()
